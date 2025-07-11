846c74f478865bce15ceb4fe5f0b42fb2c7aa30d
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-11 13:07:07 +0300

    Render Error card component

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import Form from "/components/Form";
 import MemoryCard from "/components/MemoryCard";
 import AssistiveTechInfo from "./components/AssistiveTechInfo";
 import GameOver from "./components/GameOver";
+import ErrorCard from "./components/ErrorCard";
 
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
@@ -126,7 +127,7 @@ export default function App() {
   return (
     <main>
       <h1>Memory</h1>
-      {!isGameOn && <Form handleSubmit={startGame} />}
+      {!isGameOn && !isError && <Form handleSubmit={startGame} />}
       {isGameOn && !areAllCardsMatched && (
         <AssistiveTechInfo
           emojisData={emojisData}
@@ -142,6 +143,7 @@ export default function App() {
           matchedCards={matchedCards}
         />
       )}
+      {isError && <ErrorCard handleClick={resetError} />}
     </main>
   );
 }
