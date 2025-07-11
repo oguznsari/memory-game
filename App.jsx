eb40f77d7c7e998d66f938dea7367cb4448205fc
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-11 12:21:17 +0300

    AssistiveTechInfo + accessibility with aria

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Form from "/components/Form";
 import MemoryCard from "/components/MemoryCard";
+import AssistiveTechInfo from "./components/AssistiveTechInfo";
 
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
@@ -111,6 +112,12 @@ export default function App() {
     <main>
       <h1>Memory</h1>
       {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && !areAllCardsMatched && (
+        <AssistiveTechInfo
+          emojisData={emojisData}
+          matchedCards={matchedCards}
+        />
+      )}
       {isGameOn && (
         <MemoryCard
           handleClick={turnCard}
