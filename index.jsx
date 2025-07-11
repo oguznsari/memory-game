0c48e354c790c2966d0a415d05aabab7bf7b7530
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-11 09:22:34 +0300

    fetch data from API

--- index.jsx
+++ index.jsx
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "/App";
 
-console.log("here");
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
