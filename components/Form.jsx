a0ed7c9ea9773224a1a8ed7c5d67c225739b31d6
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-11 14:26:05 +0300

    Select and Option components created and refactored

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,29 +1,15 @@
 import RegularButton from "./RegularButton";
+import Select from "./Select";
 
 export default function Form({ handleSubmit, handleChange }) {
   return (
     <div className="form-container">
+      <p className="p--regular">
+        Customize the game by selecting an emoji category and a number of memory
+        cards
+      </p>
       <form className="wrapper">
-        <div className="form__inner-wrapper">
-          <label htmlFor="category"> Select an emoji category</label>
-          <select name="category" id="category" onChange={handleChange}>
-            <option value="animals-and-nature">Animals and nature</option>
-            <option value="food-and-drink">Food and dring</option>
-            <option value="travel-and-places">Travel and places</option>
-            <option value="objects">Objects</option>
-            <option value="symbols">Symbols</option>
-          </select>
-        </div>
-        <div className="form__inner-wrapper">
-          <label htmlFor="category"> Select the number of emoji cards</label>
-          <select name="number" id="number" onChange={handleChange}>
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-            <option value="40">40</option>
-            <option value="50">50</option>
-          </select>
-        </div>
+        <Select handleChange={handleChange} />
         <RegularButton handleClick={handleSubmit}>Start Game</RegularButton>
       </form>
     </div>
